c3337d35f728143f28228da87dd283b054ec687e
lihong <lihong@study.com>
2017-11-27 19:07:52 +0800

    yike

---
+++ api/favourite.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @Author: Administrator
+ * @Date:   2017-11-21 15:25:36
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2017-11-21 15:30:09
+ */
+$fav = 'https://moment.douban.com/api/column/48/posts?alt=json&apikey=0bcf52793711959c236df76ba534c0d4&app_version=1.7.4&douban_udid=d623045db9fcb0d5243174c1bf1a675f887047c0&max_id=144114&udid=9a34d8b038ff38971050199b0c5ee9c60c6d1ca3&version=6';
+echo file_get_contents($fav);
